4b7f25696516d28be5b2082bc6009f00294111ec
Leo-Xee <jang.xmin@gmail.com>
2022-05-20 19:59:53 +0900

    feat: emotion 패키지 추가 및 글로벌 스타일 적용

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "@/app/store";
+import GlobalStyle from "@/styles/GobalStyle";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <GlobalStyle />
       <Component {...pageProps} />
     </Provider>
   );
