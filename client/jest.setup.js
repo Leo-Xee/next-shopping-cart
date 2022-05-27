04ffddfa1e72407a76b832bd1e5dcc9bd7e7a98d
Leo-Xee <jang.xmin@gmail.com>
2022-05-28 07:26:42 +0900

    feat: 테스트에 react-query 적용

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -1,14 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
 import server from "./src/mocks/server";
-import { fetch, Headers, Request, Response } from "cross-fetch";
-import AbortController from "abort-controller";
-
-/* https://github.com/reduxjs/redux-toolkit/issues/1271 */
-global.fetch = fetch;
-global.Headers = Headers;
-global.Request = Request;
-global.Response = Response;
-global.AbortController = AbortController;
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 
--- client/src/shared/utils/test-utils.tsx
+++ client/src/shared/utils/test-utils.tsx
@@ -1,10 +1,17 @@
 import { ThemeProvider } from "@emotion/react";
 import { ReactElement, ReactNode } from "react";
 import { render, RenderOptions } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import theme from "@/styles/theme";
 
+const queryClient = new QueryClient();
+
 function AllTheProviders({ children }: { children: ReactNode }) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ThemeProvider>
+  );
 }
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">) =>
