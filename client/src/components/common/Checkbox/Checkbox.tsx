0b40111a38c1b8d572b2cb6a07f509db5bfb9876
Leo-Xee <jang.xmin@gmail.com>
2022-06-03 04:24:45 +0900

    fix: Checkbox 컴포넌트의 props 수정

--- client/src/components/common/Checkbox/Checkbox.tsx
+++ client/src/components/common/Checkbox/Checkbox.tsx
@@ -3,15 +3,15 @@ import * as S from "./style";
 type CheckboxProps = {
   id: string;
   label?: string;
-  checked?: boolean;
-  onClick: () => void;
+  checked: boolean;
+  onChange: () => void;
 };
 
-function Checkbox({ id, label, checked, onClick }: CheckboxProps) {
+function Checkbox({ id, label, checked, onChange }: CheckboxProps) {
   return (
     <S.Container>
-      <S.Label htmlFor={id} onClick={onClick}>
-        <input id={id} type="checkbox" checked={checked} />
+      <S.Label htmlFor={id}>
+        <input id={id} type="checkbox" onChange={onChange} checked={checked} />
         <span />
       </S.Label>
       <S.Text>{label}</S.Text>
