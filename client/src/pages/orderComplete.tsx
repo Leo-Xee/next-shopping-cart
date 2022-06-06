c94d992c2ebd06d80c024e5525199222c98be433
Leo-Xee <jang.xmin@gmail.com>
2022-06-07 06:16:20 +0900

    feat: OrderComplete 페이지 구현

---
+++ client/src/pages/orderComplete.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+import styled from "@emotion/styled";
+import logo from "public/bm-loading.png";
+import Button from "@/components/common/Button";
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+  font-size: 3rem;
+  font-weight: 500;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`;
+
+function OrderComplete() {
+  const router = useRouter();
+
+  return (
+    <Container>
+      <Image src={logo} width="400px" height="400px" />
+      <div>결제가 완료되었습니다!! 🎉</div>
+      <ButtonContainer>
+        <Button
+          buttonName="홈으로"
+          colorType="primary"
+          size="full"
+          onClick={() => router.push("/")}
+        />
+        <Button
+          buttonName="주문상세"
+          colorType="primary"
+          size="full"
+          onClick={() => router.push("/orderDetail")}
+        />
+      </ButtonContainer>
+    </Container>
+  );
+}
+
+export default OrderComplete;
