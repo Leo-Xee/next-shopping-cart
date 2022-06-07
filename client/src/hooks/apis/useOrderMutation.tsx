ec9a45d53689a11c0e93e8bbe68802d65964950c
Leo-Xee <jang.xmin@gmail.com>
2022-06-07 22:07:23 +0900

    refactor: useOrderMutation의 위치 변경 및 옵션 추가

---
+++ client/src/hooks/apis/useOrderMutation.tsx
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "react-query";
+import { OrderItemType } from "@/@types/api";
+import orderService from "@/services/orderService";
+
+function useOrderMutation() {
+  const queryClient = useQueryClient();
+
+  const option = {
+    onSuccess: () => {
+      queryClient.invalidateQueries("/orders");
+    },
+  };
+
+  const addOrderMutation = useMutation(
+    (orderDetails: OrderItemType[]) => orderService.addOrder(orderDetails),
+    option,
+  );
+
+  return {
+    addOrder: (orderDetails: OrderItemType[]) => addOrderMutation.mutate(orderDetails),
+  };
+}
+
+export default useOrderMutation;
