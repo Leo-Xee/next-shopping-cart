a85a4a831454ba75ce0af53bdba8072768568780
Leo-Xee <jang.xmin@gmail.com>
2022-06-07 20:57:13 +0900

    test: msw의 order mockdata 변경

--- client/src/mocks/data/orders.ts
+++ client/src/mocks/data/orders.ts
@@ -26,4 +26,32 @@ export const orders: Order[] = [
       },
     ],
   },
+  {
+    id: 1653961414177,
+    orderDetails: [
+      {
+        id: 7,
+        name: "식자재왕 김말이튀김 야채맛 1kg",
+        price: 6580,
+        imageUrl: "https://cdn-mart.baemin.com/sellergoods/bulk/20211210-155432/9416-main-01.jpg",
+        quantity: 4,
+      },
+      {
+        id: 8,
+        name: "삼양 야채튀김 (60g*50±1/박스) 3kg",
+        price: 24610,
+        imageUrl:
+          "https://cdn-mart.baemin.com/sellergoods/main/8916edff-9fa9-4538-95c3-13d463f58a86.jpg",
+        quantity: 2,
+      },
+      {
+        id: 9,
+        name: "미미사 분모자 (17mm) 250G",
+        price: 1440,
+        imageUrl:
+          "https://cdn-mart.baemin.com/sellergoods/main/6ae4c431-6988-41ab-8894-7df7ee7b7cb3.jpg",
+        quantity: 1,
+      },
+    ],
+  },
 ];
