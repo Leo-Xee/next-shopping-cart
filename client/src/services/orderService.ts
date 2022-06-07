d5253719d520d08f7cc329c8cd43b32d7506d909
Leo-Xee <jang.xmin@gmail.com>
2022-06-07 17:28:16 +0900

    feat: orderService 추가

---
+++ client/src/services/orderService.ts
@@ -0,0 +1,15 @@
+import { OrderItemType } from "@/@types/api";
+import fetcher from "@/shared/utils/fetcher";
+
+const orderService = {
+  getOrders: async () => {
+    const data = await fetcher("get", "/orders");
+    return data;
+  },
+  addOrder: async (orderDetails: OrderItemType[]) => {
+    const data = await fetcher("post", "/orders", { orderDetails });
+    return data;
+  },
+};
+
+export default orderService;
