2078a7384a371c736aeb2abff5876dc52b4c14a0
Jangmin Lee <jang.xmin@gmail.com>
2022-06-18 17:51:35 +0900

    test: mockdata 수정

--- client/src/mocks/data/carts.ts
+++ client/src/mocks/data/carts.ts
@@ -33,6 +33,18 @@ export const carts: Cart[] = [
       imageUrl:
         "https://cdn-mart.baemin.com/sellergoods/main/6b95c66a-c13d-4ccd-9df5-b1af1428a225.jpg",
       quantity: 1,
+      selected: false,
+    },
+  },
+  {
+    id: 3,
+    product: {
+      id: 13,
+      name: "캔시머 페트캔(500ml)",
+      price: 31200,
+      imageUrl:
+        "https://cdn-mart.baemin.com/sellergoods/main/80c5ff67-f830-4b1e-a7e3-3f9c0ef271d1.jpg",
+      quantity: 1,
       selected: true,
     },
   },
