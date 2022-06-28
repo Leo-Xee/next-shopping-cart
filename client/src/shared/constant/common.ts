673f92ad54231863b51f20597afdd2a4739b5bb8
Leo-Xee <jang.xmin@gmail.com>
2022-06-28 18:18:44 +0900

    fix: API 엔드포인트의 조건 변경

--- client/src/shared/constant/common.ts
+++ client/src/shared/constant/common.ts
@@ -1,5 +1,5 @@
 const BASE_URL =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
     ? "http://localhost:3003"
     : "https://next-shopping-api.herokuapp.com/";
 
