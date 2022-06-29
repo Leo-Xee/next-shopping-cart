e4882bb255ff3fab6e8789d153574ee2072a5a47
Leo-Xee <jang.xmin@gmail.com>
2022-06-29 20:25:18 +0900

    feat: HTML에 lang 속성 추가

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ko">
       <Head>
         <link rel="shortcut icon" href="/favicon_2021.ico" />
         <meta name="description" content="WOOWA SHOP에서 다양한 상품들을 구매해보세요~!" />
